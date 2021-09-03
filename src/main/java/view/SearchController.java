2326570f64bf6c6d37ad1a0a1a5a512a826be326
Ralph Soika <ralph.soika@imixs.com>
2021-09-03 13:57:52 +0200

    initial import
    Issue #5

---
+++ src/main/java/view/SearchController.java
@@ -0,0 +1,53 @@
+package org.imixs.application.ui.view;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+
+import org.imixs.workflow.faces.data.ViewController;
+
+/**
+ * Simple example for a search controller searching the Imixs Fulltext Index
+ * 
+ * @author rsoika
+ */
+@Named
+@SessionScoped
+public class SearchController extends ViewController implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String input;
+	
+	
+	private static Logger logger = Logger.getLogger(SearchController.class.getName());
+
+	@Override
+	@PostConstruct
+	public void init() {
+		super.init();
+		this.setQuery("(type:\"workitem\")");
+        this.setSortBy("$modified");
+        this.setSortReverse(true);
+        this.setLoadStubs(true);
+
+	}
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+
+	
+	public void search() {
+		this.setQuery("(type:\"workitem\" OR type:\"workitemarchive\") AND ("+input+"*)");
+		
+		logger.info("serach query=" + this.getQuery());
+	}
+	
+}
